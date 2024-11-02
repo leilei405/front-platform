883ae44a7da5d29a9e8d763530fc8689379d88a0
leilei405 <1601178425@qq.com>
2024-11-03 01:40:56 +0800

    feat: ✨划分移动端首页模块

---
+++ src/views/main/components/navigation/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <pc-navigation v-if="!isMobileTerminal" />
+    <mobile-navigation v-else />
+  </div>
+</template>
+
+<script setup>
+import { isMobileTerminal } from '@/utils/flexible'
+import pcNavigation from './pc/index.vue'
+import mobileNavigation from './mobile/index.vue'
+</script>
+
+<style lang="scss" scoped></style>
