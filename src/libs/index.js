072dc9c35c8d83b472ec94f60464d78f47f81283
leilei405 <1601178425@qq.com>
2024-11-04 00:07:55 +0800

    feat: ✨vite 通用组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,5 +1,4 @@
-import svgIcon from './svg-icon/index.vue'
-import popup from './popup/index.vue'
+import { defineAsyncComponent } from 'vue'
 
 export default {
   /**
@@ -7,7 +6,19 @@ export default {
    * @param {Vue} Vue - Vue 构造函数
    */
   install(Vue) {
-    Vue.component('svg-icon', svgIcon)
-    Vue.component('popup', popup)
+    // 1. 获取当前路径下所有文件夹中的index.vue
+    const components = import.meta.glob('./*/index.vue')
+    console.log(components)
+    // 2. 循环遍历 将所有组件注册到Vue实例中
+    for (const key in components) {
+      // 2.1 获取组件名称
+      const componentName = key.replace('./', '').split('/')[0]
+
+      // 2.2 获取组件
+      const component = defineAsyncComponent(components[key])
+
+      // 3. 注册组件
+      Vue.component(componentName, component)
+    }
   }
 }
