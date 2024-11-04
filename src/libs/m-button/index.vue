b5ff01e902e30f8833cd9248686030f00ba84e71
leilei405 <1601178425@qq.com>
2024-11-04 23:11:01 +0800

    feat: ✨编写button按钮

---
+++ src/libs/m-button/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <button
+    class="text-sm text-center rounded duration-150 flex justify-center items-center"
+    :class="[
+      typeEnum[type],
+      sizeEnum[sizeKey].button,
+      {
+        'active:scale-105': isActiveAnim
+      }
+    ]"
+  >
+    <svg-icon
+      v-if="loading"
+      name="loading"
+      class="w-2 h-2 animate-spin mr-1"
+    ></svg-icon>
+
+    <svg-icon
+      v-if="icon"
+      :name="icon"
+      class="m-auto"
+      :color="iconColor"
+      :class="sizeEnum[sizeKey].icon"
+      :fillClass="iconClass"
+    ></svg-icon>
+
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+const typeEnum = {
+  primary: 'text-white bg-zinc-800 hover:bg-zinc-900 active:bg-zinc-800',
+  main: 'text-white bg-main hover:bg-zinc-900 active:bg-zinc-800',
+  info: 'text-zinc-800 bg-zinc-200 hover:bg-zinc-300 active:bg-zinc-200'
+}
+
+const sizeEnum = {
+  default: {
+    button: 'w-8 h-4 text-base',
+    icon: ''
+  },
+  'icon-default': {
+    button: 'w-4 h-4',
+    icon: 'w-1.5 h-1.5'
+  },
+  small: {
+    button: 'w-7 h-3 text-base',
+    icon: ''
+  },
+  'icon-small': {
+    button: 'w-3 h-3',
+    icon: 'w-1.5 h-1.5'
+  }
+}
+</script>
+
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  icon: String,
+  iconColor: String,
+  iconClass: String,
+
+  type: {
+    type: String,
+    default: 'main',
+    validator(val) {
+      const result = Object.keys(typeEnum).includes(val)
+      if (!result) {
+        throw new Error(
+          `type 类型错误，可选值为：${Object.keys(typeEnum).join(',')}`
+        )
+      }
+      return result
+    }
+  },
+  size: {
+    type: String,
+    default: 'default',
+    validator(val) {
+      const keys = Object.keys(sizeEnum).filter(key => !key.includes('icon'))
+      const result = keys.includes(val)
+      if (!result) {
+        throw new Error(`size 类型错误，可选值为：${keys.join(',')}`)
+      }
+      return result
+    }
+  },
+  isActiveAnim: {
+    type: Boolean,
+    default: false
+  },
+  loading: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const sizeKey = computed(() => {
+  return props.icon ? 'icon-' + props.size : props.size
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center">
       <img
         class="h-5 w-5 cursor-pointer mr-2"
-        src="http://lowcodedemo.top/%E5%BE%AE%E4%BF%A1%E5%9B%BE%E7%89%87_20241104203230.jpg"
+        src="http://lowcodedemo.top/logo.jpg"
       />
       <HeaderSearch />
       <HeaderTheme />
