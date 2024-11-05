8c8eb94fa046c1dd1a7447502d848dc9c8b38316
leilei405 <1601178425@qq.com>
2024-11-05 20:45:19 +0800

    feat: ✨完成search 组件基本功能

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,25 +1,32 @@
 <template>
   <div
+    ref="containerTarget"
     class="group relative p-0.5 rounded-xl border-white duration-500 hover:bg-blue-100/40"
   >
     <div>
-      <!-- search-icon -->
+      <!-- 前置搜索小图标 -->
       <svg-icon
         class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] left-2"
         name="search"
         color="#070707"
       />
-      <!-- search-input -->
+      <!--输入框 -->
       <input
         type="text"
         class="block w-full h-[44px] pl-4 outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide text-sm font-semibold border border-zinc-100 duration-500 focus:border-blue-300 group-hover:bg-white group-hover:border-zinc-300"
         placeholder="搜索"
+        v-model="inputValue"
+        @focus="onFocusHandler"
+        @blur="onBlurHandler"
+        @keyup.enter="onSearchHandler"
       />
 
-      <!-- search-delete -->
+      <!-- 删除按钮 -->
       <svg-icon
-        class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] right-9 cursor-pointer duration-500"
+        v-show="inputValue"
         name="input-delete"
+        class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] right-9 cursor-pointer duration-500"
+        @click="onClearClick"
       />
 
       <!-- 分割线  -->
@@ -33,17 +40,72 @@
         type="main"
         icon="search"
         iconColor="#ffffff"
+        @click="onSearchHandler"
       />
     </div>
+
+    <!-- 搜索出来的下拉内容区域 -->
     <transition name="slide">
       <div
+        v-if="$slots.dropdown"
+        v-show="isFocus"
         class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[59px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl"
       ></div>
     </transition>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import { useVModel, onClickOutside } from '@vueuse/core'
+// 1. 输入内容实现双向数据绑定
+const props = defineProps({
+  modelValue: {
+    type: String,
+    required: true
+  }
+})
+
+// 2. 事件处理
+const emits = defineEmits([
+  'update:modelValue',
+  'search',
+  'clear',
+  'input',
+  'focus',
+  'blur'
+])
+const inputValue = useVModel(props)
+
+// 3. 删除
+const onClearClick = () => {
+  inputValue.value = ''
+  emits('clear', '')
+}
+
+// 4. 触发搜索
+const onSearchHandler = () => {
+  emits('search', inputValue.value)
+}
+
+// 5. 下拉框的显示和隐藏
+const isFocus = ref(false)
+const onFocusHandler = () => {
+  isFocus.value = true
+  emits('focus')
+}
+
+// 6. 区域外的事件处理
+const containerTarget = ref(null)
+onClickOutside(containerTarget, () => {
+  isFocus.value = false
+})
+
+// 监听输出事件
+watch(inputValue, val => {
+  emits('input', val)
+})
+</script>
 
 <style lang="scss" scoped>
 .slide-enter-active,
--- src/views/layout/components/header/headerSearch/index.vue
+++ src/views/layout/components/header/headerSearch/index.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="w-full">
-    <search />
+    <search v-model="inputValue">
+      <template #dropdown>
+        <div class="w-full h-20 bg-red-500">drop</div>
+      </template>
+    </search>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const inputValue = ref('')
+</script>
 
 <style lang="scss" scoped></style>
