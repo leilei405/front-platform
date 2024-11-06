7fda92fa37f20bae0ebb211e608c7113c0d9b80b
leilei405 <1601178425@qq.com>
2024-11-06 20:50:16 +0800

    feat: ✨pC 下 navigationBar 展开收起样式处理 选中状态处理

--- src/views/main/components/navigation/pc/index.vue
+++ src/views/main/components/navigation/pc/index.vue
@@ -2,16 +2,26 @@
   <div class="bg-white sticky top-0 left-0 w-full z-10">
     <ul
       class="w-[800px] relative flex flex-wrap justify-center overflow-x-auto px-[10px] py-1 text-zinc-600 duration-300 overflow-hidden mx-auto"
+      :class="[isOpenCategory ? 'h-[206px]' : 'h-[56px]']"
     >
       <div
         class="absolute right-1 bottom-1 z-20 p-1 rounded cursor-pointer duration-200 hover:bg-zinc-200"
+        @click="toggleCategory"
       >
-        <svg-icon name="unfold" class="w-1 h-1" fillClass=" fill-zinc-900" />
+        <svg-icon
+          :name="isOpenCategory ? 'fold' : 'unfold'"
+          class="w-1 h-1"
+          fillClass=" fill-zinc-900"
+        />
       </div>
       <li
-        v-for="item in $store.getters.categoryData"
+        v-for="(item, index) in $store.getters.categoryData"
         :key="item.id"
         class="shrink-0 px-1.5 py-0 z-10 duration-200 text-zinc-900 text-base font-bold h-4 leading-4 cursor-pointer hover:bg-zinc-200 rounded mr-1 mb-1"
+        :class="{
+          'text-zinc-900 bg-zinc-200': selectedCurrentIndex === index
+        }"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -19,6 +29,20 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const isOpenCategory = ref(false)
+
+// 展开收起
+const toggleCategory = () => {
+  isOpenCategory.value = !isOpenCategory.value
+}
+
+// 选中状态处理
+const selectedCurrentIndex = ref(0)
+const onItemClick = index => {
+  selectedCurrentIndex.value = index
+}
+</script>
 
 <style lang="scss" scoped></style>
