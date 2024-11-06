30d5e88b6b75d59f5381df06d5e7d3e479612973
leilei405 <1601178425@qq.com>
2024-11-06 21:10:28 +0800

    feat: ✨将 将每次从接口获取到的数据  进行缓存  在下次运行时 把缓存数据作为初始数据

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 import getters from './getters'
 import category from './modules/category'
 
@@ -7,7 +8,14 @@ const store = createStore({
   modules: {
     // 注册模块
     category
-  }
+  },
+  plugins: [
+    createPersistedState({
+      // 本地存储插件
+      key: 'category',
+      paths: ['category']
+    })
+  ]
 })
 
 export default store
--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -9,6 +9,7 @@ export default {
       // 1.categoryData 给一组初始数据
       // 2.让服务端获取数据 替换初始数据
       // 3.防止初始化数据太老 我们每次把 获取到的新数据 都作为下一次的初始化数据
+      // 将每次从接口获取到的数据  进行缓存  在下次运行时 把缓存数据作为初始数据
       categoryData: initCategoryList
     }
   },
