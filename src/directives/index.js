336b0d8f55eb2330b13e01deb60a40b3715fa58c
leilei405 <1601178425@qq.com>
2024-11-09 17:24:00 +0800

    feat: ✨全局指令注册

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,11 @@
-import lazy from './modules/lazy'
-
+// 全局指令注册
 export default {
-  install(Vue) {
-    Vue.directive('lazy', lazy)
+  async install(Vue) {
+    const directives = import.meta.globEager('./modules/*.js')
+    for (const [key, value] of Object.entries(directives)) {
+      const arr = key.split('/')
+      const name = arr[arr.length - 1].replace('.js', '')
+      Vue.directives(name, value.default)
+    }
   }
 }
