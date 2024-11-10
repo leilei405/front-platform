af39477ec1a6605249f2e6da4522555c7432d95e
leilei405 <1601178425@qq.com>
2024-11-10 13:41:20 +0800

    feat: ✨搜索关键字高亮

--- src/views/layout/components/header/headerSearch/hint.vue
+++ src/views/layout/components/header/headerSearch/hint.vue
@@ -4,10 +4,9 @@
       v-for="(item, index) in hintData"
       :key="index"
       class="py-1 pl-1 text-base font-bold text-zinc-500 rounded cursor-pointer duration-300 hover:bg-zinc-200 dark:hover:bg-zinc-900"
+      v-html="highlight(item)"
       @click="onItemClick(item)"
-    >
-      {{ item }}
-    </div>
+    ></div>
   </div>
 </template>
 321
@@ -22,6 +21,7 @@ const props = defineProps({
     required: true
   }
 })
+
 // 获取搜索数据
 const hintData = ref([])
 const getHintData = async () => {
@@ -29,12 +29,21 @@ const getHintData = async () => {
   const { result } = await getHintList(props.searchText)
   hintData.value = result
 }
+
 // 监听searchText 输入框变化 防抖 500ms 后请求数据
 watchDebounced(() => props.searchText, getHintData, {
   immediate: true,
   debounce: 500
 })
+
 const onItemClick = item => {
   emits('itemClick', item)
+}
+
+// 处理关键字高亮
+const highlight = text => {
+  const htmlDanger = `<span class="text-red-600 dark:text-red-600">${props.searchText}</span>`
+  const reg = new RegExp(props.searchText, 'gi')
+  return text.replace(reg, htmlDanger)
 }
 </script>
