ba6e93fae2b037da5d6895df8ce03d7d34bdf917
leilei405 <1601178425@qq.com>
2024-11-15 21:41:13 +0800

    feat: ✨添加移动端路由 代码格式调整

--- src/router/modules/mobile-routes.js
+++ src/router/modules/mobile-routes.js
@@ -8,5 +8,10 @@ export default [
     path: '/detail/:id',
     name: 'detail',
     component: () => import('@/views/pins/index.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login-register/login/index.vue')
   }
 ]
